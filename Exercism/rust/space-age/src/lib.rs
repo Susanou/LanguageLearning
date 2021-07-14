9ac14fd59130bc3271c5884ecc74814f2a9a8df5
Fukurou <caos2804@gmail.com>
2021-07-14 17:32:18 -0400

    bash exercises + rust

--- Exercism/rust/space-age/src/lib.rs
+++ Exercism/rust/space-age/src/lib.rs
@@ -1,38 +1,46 @@
 // The code below is a stub. Just enough to satisfy the compiler.
 // In order to pass the tests you can add-to or change any of this code.
 
+const EARTH_YEAR_IN_SEC: f64 = 31_557_600.;
+
 #[derive(Debug)]
-pub struct Duration;
+pub struct Duration{
+    sec: f64
+}
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        unimplemented!("s, measured in seconds: {}", s)
+        Duration{sec: s as f64}
     }
 }
 
 pub trait Planet {
-    fn years_during(d: &Duration) -> f64 {
-        unimplemented!(
-            "convert a duration ({:?}) to the number of years on this planet for that duration",
-            d,
-        );
+    const EARTH_TO_PLANET: f64;
+
+    fn years_during(d: &Duration) -> f64{
+        d.sec as f64 / (EARTH_YEAR_IN_SEC*Self::EARTH_TO_PLANET)
     }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
+macro_rules! impl_Planet {
+    ($(($p:ident, $r:literal)), *) =>{
+        $(
+            pub struct $p;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+            impl Planet for $p {
+                const EARTH_TO_PLANET: f64 = $r;
+            }
+        )*
+    };
+}
+
+impl_Planet!(
+    (Mercury, 0.2408467),
+    (Venus, 0.61519726),
+    (Earth, 1.0),
+    (Mars, 1.8808158),
+    (Jupiter, 11.862615),
+    (Saturn, 29.447498),
+    (Uranus, 84.016846),
+    (Neptune, 164.79132)
+);
