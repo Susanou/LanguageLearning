e20b315e902e230b66be01aa3b4da01117e839ab
Fukurou <caos2804@gmail.com>
2021-11-15 23:02:39 -0500

    rust projects

--- rust/ProgrammingRustBook/quickreplace/src/main.rs
+++ rust/ProgrammingRustBook/quickreplace/src/main.rs
@@ -1,6 +1,7 @@
 use text_colorizer::*;
 use std::env;
 use std::fs;
+use regex::Regex;
 
 #[derive(Debug)]
 struct Arguments{
@@ -34,6 +35,13 @@ fn parse_args() -> Arguments{
     }
 }
 
+fn replace(target: &str, replacement: &str, text: &str)
+    -> Result<String, regex::Error>
+{
+    let regex = Regex::new(target)?;
+    Ok(regex.replace_all(text, replacement).to_string())
+}
+
 fn main() {
     let args = parse_args();
     
@@ -46,7 +54,16 @@ fn main() {
         }
     };
 
-    match fs::write(&args.output, &data) {
+    let replaced_data = match replace(&args.target, &args.replacement, &data){
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{} failed to replace text: {:?}",
+                "Error:".red().bold(), e);
+            std::process::exit(1);
+        }
+    };
+
+    match fs::write(&args.output, &replaced_data) {
         Ok(_) => {},
         Err(e) => {
             eprintln!("{} failed to write to file '{}': {:?}",
