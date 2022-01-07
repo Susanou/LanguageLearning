feefe5175897525fdbd00b6c46ad2c62df27a5b2
Fukurou <caos2804@gmail.com>
2022-01-07 23:28:49 +0100

    added attacks

--- rust/HandsOnRust/dungeon_crawler/src/systems/mod.rs
+++ rust/HandsOnRust/dungeon_crawler/src/systems/mod.rs
@@ -3,12 +3,12 @@ use crate::prelude::*;
 mod player_input;
 mod map_render;
 mod entity_render;
-mod collisions;
 mod random_move;
 mod end_turn;
 mod movement;
 mod hud;
 mod tooltips;
+mod combat;
 
 pub fn build_input_scheduler() -> Schedule {
     Schedule::builder()
@@ -23,9 +23,9 @@ pub fn build_input_scheduler() -> Schedule {
 
 pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
-        .add_system(movement::movement_system())
+        .add_system(combat::combat_system())
         .flush()
-        .add_system(collisions::collisions_system())
+        .add_system(movement::movement_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
@@ -38,9 +38,9 @@ pub fn build_monster_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(random_move::random_move_system())
         .flush()
-        .add_system(movement::movement_system())
+        .add_system(combat::combat_system())
         .flush()
-        .add_system(collisions::collisions_system())
+        .add_system(movement::movement_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
