da08d2914cfa5495cbfdcb27c3187a52e88aac47
Fukurou <caos2804@gmail.com>
2022-02-05 00:38:03 -0500

    waiting strategy

--- rust/HandsOnRust/dungeon_crawler/src/systems/player_input.rs
+++ rust/HandsOnRust/dungeon_crawler/src/systems/player_input.rs
@@ -1,8 +1,10 @@
 use crate::prelude::*;
 
 #[system]
-#[write_component(Point)]
+#[read_component(Enemy)]
+#[read_component(Point)]
 #[read_component(Player)]
+#[write_component(Health)]
 pub fn player_input(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
@@ -27,6 +29,8 @@ pub fn player_input(
             .unwrap();
 
         let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+
+        let mut did_something = false;
         if delta.x != 0 || delta.y != 0{
             let mut hit_something = false;
             enemies
@@ -36,6 +40,7 @@ pub fn player_input(
                 })
                 .for_each(|(entity, _)| {
                     hit_something = true;
+                    did_something = true;
 
                     commands.push(((), WantsToAttack{
                         attacker: player_entity,
@@ -44,11 +49,23 @@ pub fn player_input(
                 });
             
             if !hit_something {
+                did_something = true;
                 commands
                     .push(((), WantsToMove{
                         entity: player_entity,
                         destination
                     }));
+            }
+
+        }
+
+        if !did_something {
+            if let Ok(mut health) = ecs
+                .entry_mut(player_entity)
+                .unwrap()
+                .get_component_mut::<Health>()
+            {
+                health.current = i32::min(health.max, health.current+1);
             }
         }
 
