e9696c100449becec8c9cdd038cab8b3db86b849
Fukurou <caos2804@gmail.com>
2022-02-15 23:28:30 -0500

    new libraries and files

--- OpenGL/modifications/main.cpp
+++ OpenGL/modifications/main.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
 
 #include"shaderClass.h"
 #include"VAO.h"
