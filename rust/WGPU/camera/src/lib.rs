c3c8d843e4ee0136a4290af1860054f29306561c
Fukurou <caos2804@gmail.com>
2022-08-18 17:21:08 +0200

    bug correction

--- rust/WGPU/camera/src/lib.rs
+++ rust/WGPU/camera/src/lib.rs
@@ -84,28 +84,20 @@ struct Camera {
     aspect: f32,
     fovy: f32,
     znear: f32,
-    zfar: f32
+    zfar: f32,
 }
 
 impl Camera {
     fn build_view_projection_matrix(&self) -> cgmath::Matrix4<f32> {
-        //world position and rotation of the camera ( = transform ^ -1)
         let view = cgmath::Matrix4::look_at_rh(self.eye, self.target, self.up);
-
-        // matrix to give depth feel
         let proj = cgmath::perspective(cgmath::Deg(self.fovy), self.aspect, self.znear, self.zfar);
-
-        return OPENGL_TO_WGPU_MATRIX * proj * view;
+        proj * view
     }
 }
 
-//needed for Rust to store the data correctlt for shaders
 #[repr(C)]
-//all of this is needed so that we can store in a buffer
 #[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
 struct CameraUniform {
-    //can't use cgmath and bytemuck at the same time
-    // so we convert the Matrix4 to a 4x4 f32 array
     view_proj: [[f32; 4]; 4],
 }
 
@@ -118,12 +110,14 @@ impl CameraUniform {
     }
 
     fn update_view_proj(&mut self, camera: &Camera) {
-        self.view_proj = camera.build_view_projection_matrix().into();
+        self.view_proj = (OPENGL_TO_WGPU_MATRIX * camera.build_view_projection_matrix()).into();
     }
 }
 
 struct CameraController {
     speed: f32,
+    is_up_pressed: bool,
+    is_down_pressed: bool,
     is_forward_pressed: bool,
     is_backward_pressed: bool,
     is_left_pressed: bool,
@@ -134,6 +128,8 @@ impl CameraController {
     fn new(speed: f32) -> Self {
         Self {
             speed,
+            is_up_pressed: false,
+            is_down_pressed: false,
             is_forward_pressed: false,
             is_backward_pressed: false,
             is_left_pressed: false,
@@ -141,21 +137,29 @@ impl CameraController {
         }
     }
 
-    fn process_event(&mut self, event: &WindowEvent) -> bool {
+    fn process_events(&mut self, event: &WindowEvent) -> bool {
         match event {
-            WindowEvent::KeyboardInput { 
-                input: KeyboardInput {
-                    state,
-                    virtual_keycode: Some(keycode),
-                    ..
-                },
+            WindowEvent::KeyboardInput {
+                input:
+                    KeyboardInput {
+                        state,
+                        virtual_keycode: Some(keycode),
+                        ..
+                    },
                 ..
             } => {
                 let is_pressed = *state == ElementState::Pressed;
                 match keycode {
-
+                    VirtualKeyCode::Space => {
+                        self.is_up_pressed = is_pressed;
+                        true
+                    }
+                    VirtualKeyCode::LShift => {
+                        self.is_down_pressed = is_pressed;
+                        true
+                    }
                     VirtualKeyCode::W | VirtualKeyCode::Up => {
-                        self.is_forward_pressed = true;
+                        self.is_forward_pressed = is_pressed;
                         true
                     }
                     VirtualKeyCode::A | VirtualKeyCode::Left => {
@@ -179,12 +183,12 @@ impl CameraController {
 
     fn update_camera(&self, camera: &mut Camera) {
         use cgmath::InnerSpace;
-
         let forward = camera.target - camera.eye;
         let forward_norm = forward.normalize();
         let forward_mag = forward.magnitude();
 
-        //prevent glitches when camera is too close from origin of scene
+        // Prevents glitching when camera gets too close to the
+        // center of the scene.
         if self.is_forward_pressed && forward_mag > self.speed {
             camera.eye += forward_norm * self.speed;
         }
@@ -192,22 +196,20 @@ impl CameraController {
             camera.eye -= forward_norm * self.speed;
         }
 
-        let right = forward_norm.cross(camera.up); // left is -right vec
+        let right = forward_norm.cross(camera.up);
 
-        // redo radius calculations in case forward/backward is pressed
+        // Redo radius calc in case the up/ down is pressed.
         let forward = camera.target - camera.eye;
-        let forward_mag = forward.magnitude(); 
+        let forward_mag = forward.magnitude();
 
         if self.is_right_pressed {
-            // rescale the distance between the target and eye
-            // that was it doesn't change
-            // The eye still lies on the circle made by the target and the eye
-            camera.eye = camera.target - 
-                (forward + right * self.speed).normalize() * forward_mag;
+            // Rescale the distance between the target and eye so
+            // that it doesn't change. The eye therefore still
+            // lies on the circle made by the target and eye.
+            camera.eye = camera.target - (forward + right * self.speed).normalize() * forward_mag;
         }
         if self.is_left_pressed {
-            camera.eye = camera.target - 
-                (forward - right * self.speed).normalize() * forward_mag;
+            camera.eye = camera.target - (forward - right * self.speed).normalize() * forward_mag;
         }
     }
 }
@@ -491,7 +493,7 @@ impl State {
     }
 
     fn input(&mut self, event: &WindowEvent) -> bool {
-        self.camera_controller.process_event(event)
+        self.camera_controller.process_events(event)
     }
 
     fn update(&mut self) {
